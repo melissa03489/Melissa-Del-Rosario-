78d4cf1d1c5f9ad432807dddd4009b69eced8c4b
melissa03489 <delrosariomelissa918@gmail.com>
2024-10-02 19:24:29 +0800

    Update ageCheck.js

--- ageCheck.js
+++ ageCheck.js
@@ -1,4 +1,4 @@
-if statement activity 
+//if statement activity 
 let age = prompt("Enter your age:");
 if (age < 13) {
   console.log("You are a child.");
@@ -9,7 +9,7 @@ if (age < 13) {
 }
 
 
-if else if activity 
+//if else if activity 
 let score = prompt("Enter your score:");
 if (score >= 90) {
   console.log("Your grade is: Excellent.");
@@ -21,3 +21,33 @@ if (score >= 90) {
   console.log("Your grade is: Needs Improvement.");
 }
 
+// switch statement activity 
+// Prompt the user to enter a day of the week
+const day = prompt("Enter a day of the week:Monday");
+
+// Use a switch statement to map each day to a specific task to ensure the input is case-insensitive.
+switch (day.toLowerCase()) {
+  case "monday":
+    console.log("Your task for Monday is: Finish assignments.");
+    break;
+  case "tuesday":
+    console.log("Your task for Tuesday is: Attend a class.");
+    break;
+  case "wednesday":
+    console.log("Your task for Wednesday is: washing clothes.");
+    break;
+  case "thursday":
+    console.log("Your task for Thursday is: FReview notes.");
+    break;
+  case "friday":
+    console.log("Your task for Friday is: Summary reviewer.");
+    break;
+  case "saturday":
+    console.log("Your task for Saturday is: Take a break.");
+    break;
+  case "sunday":
+    console.log("Your task for Sunday is: Working day.");
+    break;
+  default:
+    console.log("Finish assignments.");
+}
