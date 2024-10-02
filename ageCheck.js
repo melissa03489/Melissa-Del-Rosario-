05eee30972c333de55650e1acebede0dc9a36b71
melissa03489 <delrosariomelissa918@gmail.com>
2024-10-02 09:22:22 +0800

    Create ageCheck.js

---
+++ ageCheck.js
@@ -0,0 +1,23 @@
+if statement activity 
+let age = prompt("Enter your age:");
+if (age < 13) {
+  console.log("You are a child.");
+} else if (age >= 13 && age <= 19) {
+  console.log("You are a teenager.");
+} else {
+  console.log("You are an adult.");
+}
+
+
+if else if activity 
+let score = prompt("Enter your score:");
+if (score >= 90) {
+  console.log("Your grade is: Excellent.");
+} else if (score >= 80 && score <= 89) {
+  console.log("Your grade is: Good.");
+} else if (score >= 70 && score <= 79) {
+  console.log("Your grade is: Fair.");
+} else {
+  console.log("Your grade is: Needs Improvement.");
+}
+
