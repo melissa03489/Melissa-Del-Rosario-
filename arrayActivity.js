bd3f0e70a00e186df87f6ea2b47836cbdb8b9d77
melissa03489 <delrosariomelissa918@gmail.com>
2024-10-16 21:00:41 +0800

    Update arrayActivity.js

--- arrayActivity.js
+++ arrayActivity.js
@@ -2,4 +2,5 @@
 let favSinger = ("moira","adie","ben&ben");
 console.log ["moira"]
 let favNumber = (08,03,19,05)
-let mixedArr = ["hello"],["john"],123,true]
+let mixedArr = ["hello",["john"],123,true]
+console.log [["hello",["john"],123,true]]
