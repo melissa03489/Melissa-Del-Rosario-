87cb2a030be50a4554c6c51b90d88520fd611968
melissa03489 <delrosariomelissa918@gmail.com>
2024-10-16 22:37:37 +0800

    Update arrayActivity.js

--- arrayActivity.js
+++ arrayActivity.js
@@ -7,3 +7,12 @@ console.log [["hello",["john"],123,true]]
 
 // activity 2
 let arr1 = [("Cecile","Lone")], arr2 = [("Emil","Tobias","Linos")];
+let fruits.push() = ["Kiwi","Banana","Orange","Apple","Mango"]
+let array.unshift() = arr1 = [1,2,3,4,5]
+let fruits.pop() = ["Banana","Orange","Apple"]
+let array.shift() = array2 = [2,3]
+let sort() = ["Apple","Banana","Mango","Orange"]
+let slice() = 
+let splice() = 
+
+//Activity 3
